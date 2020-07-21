9d111b23eabaa108aca62de101a5bb009f37cd6b
Savas.US <savas.us21@outlook.com>
2020-07-22 01:45:47 +0300

    first commit

---
+++ DigiturkCase/Controllers/ArticleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigiturkCase.Controllers
+{
+    [Route("api/Article")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        public string Get()
+        {
+
+            return "saddas";
+        }
+    }
+}
