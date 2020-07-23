4c03bac78d3f8d4ba2441794f973acbdc470983d
Savas.US <savas.us21@outlook.com>
2020-07-23 15:57:37 +0300

    API test için Swagger entegre edildi.

--- DigiturkCase/Startup.cs
+++ DigiturkCase/Startup.cs
@@ -14,6 +14,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
 
 namespace DigiturkCase
 {
@@ -32,6 +34,7 @@ namespace DigiturkCase
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddTransient<IArticleDal, EfArticleDal>();
             services.AddAutoMapper(typeof(AutoMapperHelper));
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "DigiturkCase", Version = "v1" }); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +51,8 @@ namespace DigiturkCase
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "DigiturkCase V1"); });
         }
     }
 }
